063ca4f14afbd96ee68d97cc48e7bcee1947e74f
Ivan <Ivan@Ivan-PC>
2013-05-07 12:07:16 -0300

    ok

--- teoriadelainfo/src/main.java
+++ teoriadelainfo/src/main.java
@@ -1,12 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
 
 public class main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("prueba.jpg"));
+		} catch (IOException e) {
+		}
+	if (img!= null)
+		System.out.println(img.getHeight());
+	
+	
+	
+	
+	
 	}
 
 }
