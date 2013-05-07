5f38669d3008ef278aa0f7ea29a927fa0db18de6
Ivan <Ivan@Ivan-PC>
2013-05-07 17:37:21 -0300

    ok

--- teoriadelainfo/src/main.java
+++ teoriadelainfo/src/main.java
@@ -4,18 +4,32 @@ import javax.imageio.ImageIO;
 
 
 public class main {
+	public static long getCoorrelacion(int vector[],int vector2[])
+	{
+		long suma=0;
+		for (int i=0;i<vector.length;i++)
+			suma=suma+((vector[i]& 0x00ffffff)*(vector2[i]& 0x00ffffff));
+		return (suma/vector.length);
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File("prueba.jpg"));
+		    img = ImageIO.read(new File("actorPrincipal.bmp"));
 		} catch (IOException e) {
 		}
-	if (img!= null)
-		System.out.println(img.getHeight());
-	
-	
-	
+		int h=img.getHeight();
+		int w=img.getWidth();
+		int [] rgbArray = new int[w*h];
+		img.getRGB(0, 0, w,h, rgbArray, 0, 0);
+		
+		//int pixel=img.getRGB(1, 1)& 0x00ffffff;//elimino el alpha
+		
+		System.out.println(getCoorrelacion(rgbArray,rgbArray));
+		
 	
 	
 	}
