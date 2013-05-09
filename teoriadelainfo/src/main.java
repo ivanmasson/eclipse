57b2a8b7b6a1241b638985d3ee6402ae7c4d0645
Roman <Roman@Roman>
2013-05-09 16:48:01 -0300

    ok

--- teoriadelainfo/src/main.java
+++ teoriadelainfo/src/main.java
@@ -28,10 +28,14 @@ public class main {
 		}
 		int h=img.getHeight();
 		int w=img.getWidth();
+		
 		int [][] matriz = new int[w][h];
-		int [] rgbArray = new int[w*h];
+		//int [] rgbArray = new int[w*h];
+		
 		Raster r=img.getData();
-		img.getRGB(0, 0, w,h, rgbArray, 0, 0);
+		
+	//	img.getRGB(0, 0, w,h, rgbArray, 0, 0);
+		
 		for(int i=0; i<w;i++)
 			for(int j=0; j<h;j++)
 				matriz[i][j]=r.getSample(i, j, 0);
