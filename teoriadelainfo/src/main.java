f9c7e9b7b79ea5ccb32aa1e8085a339d4c2c9358
Ivan <Ivan@Ivan-PC>
2013-05-13 21:57:45 -0300

    modif

--- teoriadelainfo/src/main.java
+++ teoriadelainfo/src/main.java
@@ -15,16 +15,11 @@ public class main {
 	
 	public static void main(String[] args) {
 		
-		inciso1 i1= new inciso1();
+		//inciso1 i1= new inciso1();
 		
 		CompresionImagen c1 = new CompresionImagen();
-		//DescompresionImagen d1 = new DescompresionImagen();
-		
-		
-		
-	
-		
-		
+		DescompresionImagen d1 = new DescompresionImagen();
+
 	
 	
 	}
